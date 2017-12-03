fa3ac7eb42449b55e2f97d6bdde42ca651153b8d
Tatsuya Ichikawa <>
2017-12-03 23:44:22 +0900

    Set routing for child_task

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'tasks#index'
-  resources :tasks
+  resources :tasks do
+    resources :child_tasks, only: [:create, :destroy, :edit]
+  end
 
   # get 'effects#index'
   # get 'article'=> 'effects#article'
