b82f75d4990037d7b13c40fdd9b7cd1078f3f2f6
Tatsuya Ichikawa <>
2017-12-04 08:35:04 +0900

    Edit child_tasks_controller.rb for setting action

--- app/controllers/child_tasks_controller.rb
+++ app/controllers/child_tasks_controller.rb
@@ -1,2 +1,23 @@
 class ChildTasksController < ApplicationController
+
+  def create
+    @task = Task.find(params[:task_id])
+    @child_task = @task.child_tasks.create(child_task_params)
+    redirect_to root_path
+  end
+
+  def edit
+  end
+
+  def destroy
+    @child_task = Child_task.find(params[:id])
+    @child_task.destroy
+    redirect_to root_path
+  end
+
+  private
+
+    def child_task_params
+      params.require(:child_task).permit(:title)
+    end
 end
