580fa12a209e3e15724966befbcf001dd4bb353d
Tatsuya Ichikawa <>
2017-12-07 12:49:19 +0900

    Correct bug of sort_action

--- app/assets/javascripts/task.js
+++ app/assets/javascripts/task.js
@@ -3,7 +3,7 @@ $(function(){
     $(this).parent().parent().parent().next('.form-controll').slideToggle(300);
   });
   $('.task-box__name').on('click', function(){
-    $(this).parent().parent().next().toggle('slow');
+    $(this).parent().next().next().toggle('slow');
   });
   // $('child_task-form').on('submit', function(){
   //   console.log("aaa");
