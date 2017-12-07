95e58b490975085fe9ee5b0bcefbf3662fb90129
Tatsuya Ichikawa <>
2017-12-07 09:16:33 +0900

    Set routing for status managing by checkbox

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :child_tasks, only: [:create, :destroy, :edit]
   end
 
+  post 'tasks/:task_id/child_tasks/:id/toggle' => 'child_tasks#toggle'
+
   # get 'effects#index'
   # get 'article'=> 'effects#article'
 end
