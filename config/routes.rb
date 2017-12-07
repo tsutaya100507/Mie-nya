9b41abe8596a1a8d1765b6f9c49e9dcde45d3efb
Tatsuya Ichikawa <>
2017-12-07 09:56:41 +0900

    Set routing for sort

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks do
     resources :child_tasks, only: [:create, :destroy, :edit]
+    put :sort
   end
 
   post 'tasks/:task_id/child_tasks/:id/toggle' => 'child_tasks#toggle'
