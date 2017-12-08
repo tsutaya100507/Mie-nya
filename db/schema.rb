9c7011beb5841ad895d64961ba360e6fc31aa41a
Tatsuya Ichikawa <>
2017-12-08 17:56:39 +0900

    Add index unique true to email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171208063043) do
+ActiveRecord::Schema.define(version: 20171208083444) do
 
   create_table "child_tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20171208063043) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
   add_foreign_key "child_tasks", "tasks"
