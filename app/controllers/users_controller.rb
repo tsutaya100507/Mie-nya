6e946ad1cd9700f3c9b0b9d26fab3d98b07a8dc8
Tatsuya Ichikawa <>
2017-12-09 17:52:53 +0900

    Add log_in to create_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      # signupと同時にlogin
+      log_in @user
       flash[:success] = "Welcome to the Mienya!"
       # ↓ redirect_to user_url(@user)
       redirect_to root_url
