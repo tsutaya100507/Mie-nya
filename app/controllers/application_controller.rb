d9cf9ac66ab74d6c97ffbc398cce62a639216f9c
Tatsuya Ichikawa <>
2017-12-11 01:52:40 +0900

    Set before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # ↓ SessionsHelperを全コントローラから使えるようにする
   include SessionsHelper
+  before_action :logged_in_user
+
+  def logged_in_user
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
